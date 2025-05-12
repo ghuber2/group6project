1a8b41d5792ebfe3516c18b40b00e456bd0cf2ae
Elijah Mari <158182057+emari1@users.noreply.github.com>
2025-05-12 07:22:57 -0400

    Update Post.jsx

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,14 +1,28 @@
-function Post() {
-    return (
-        <div class="post-header">
-            <div class="avatar">TU</div>
-            <div class="post-info">
-                <strong>Post 1</strong><br></br>
-                Post by: XXX <br></br>
-                Location: XXX <br></br>
-            </div>
-        </div>
-    )
+// src/components/Post.jsx
+import React from 'react';
+
+function Post({ post }) {
+  const { title, description, date, time, lat, lng, username } = post;
+  return (
+    <div className="post-header" style={{ marginBottom: 16 }}>
+      <div className="avatar" style={{
+          width: 40, height: 40, borderRadius: '50%',
+          background: '#ddd', display: 'flex',
+          alignItems: 'center', justifyContent: 'center',
+          fontWeight: 700
+        }}>
+        {username.charAt(0).toUpperCase()}
+      </div>
+      <div className="post-info" style={{ marginLeft: 12 }}>
+        <strong>{title}</strong><br/>
+        {description}<br/>
+        <small>
+          {date} @ {time}<br/>
+          Location: {lat.toFixed(4)}, {lng.toFixed(4)}
+        </small>
+      </div>
+    </div>
+  );
 }
 
 export default Post;
