d2f3592cf020935a52bb9c134993f32eccfc1e44
Elijah Mari <158182057+emari1@users.noreply.github.com>
2025-05-12 07:20:38 -0400

    Update MapModal.jsx

--- src/components/MapModal.jsx
+++ src/components/MapModal.jsx
@@ -8,13 +8,13 @@ const MapModal = ({ post, onClose }) => {
             background: '#fff', padding: 20, border: '1px solid #ccc',
             borderRadius: 8, zIndex: 1000
         }}>
-            <h3>{post.title}</h3>
-            <p>{post.description}</p>
-            <p>{post.date} @ {post.time}</p>
+            <h3 className="map-post-info">{post.title}</h3>
+            <p className="map-post-info">{post.description}</p>
+            <p className="map-post-info">{post.date} @ {post.time}</p>
             <div style={{ height: '500px', width: '100%', marginTop: 10 }}>
-                <PostMap lat={post.lat} lng={post.lng} />
+                <PostMap lat={post.lat} lng={post.lng} className="map-modal-map"/>
             </div>
-            <button style={{ marginTop: 10 }} onClick={onClose}>Close</button>
+            <button id="map-close-button"style={{ marginTop: 10 }} onClick={onClose}>Close</button>
         </div>
     );
 };
